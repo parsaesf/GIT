bcf863d7ca55e56bd91938b08a7766de8f26a483
parsaesf <118264118+parsaesf@users.noreply.github.com>
2025-09-05 21:45:06 +0330

    fixed

--- core/settings.py
+++ core/settings.py
@@ -109,3 +109,5 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
