9f34d96998e01dd2c7ba1fcf309336842c2ba975
parsaesf <118264118+parsaesf@users.noreply.github.com>
2025-09-05 21:43:47 +0330

    error

--- core/settings.py
+++ core/settings.py
@@ -109,5 +109,3 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
