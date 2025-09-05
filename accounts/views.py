60d224d3b9c244036d7a2bf4d86a128948b86ac7
parsaesf <118264118+parsaesf@users.noreply.github.com>
2025-09-05 19:03:47 +0330

    from develop

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# accounts/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home Page from develop")
