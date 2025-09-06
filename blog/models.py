8e93b8653d1e511dbef3248b5407dbb740377ced
parsaesf <118264118+parsaesf@users.noreply.github.com>
2025-09-06 10:13:15 +0330

    commit 3

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    desc = models.TextField(max_length=200,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+
+
 def home():
-    print("this is home")
+    return HttpResponse("this is home")
 
-def index():
-    print("this is index")
-
+def about():
+    return HttpResponse("this is about page")
