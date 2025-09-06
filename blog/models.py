271707385318cf4e98f3a8a62f319fde5f7c3be2
parsaesf <118264118+parsaesf@users.noreply.github.com>
2025-09-06 11:01:26 +0330

    back

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,2 @@
 from django.db import models
 
-class Post(models.Model):
-    title = models.CharField(max_length=30)
-    desc = models.TextField(max_length=200,blank=True,null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
